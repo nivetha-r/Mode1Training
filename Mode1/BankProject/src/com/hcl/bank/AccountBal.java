1b4b7af7bf7dc570c5c5053159224342cf2effe6
nivetha-r <55445156+nivetha-r@users.noreply.github.com>
2019-10-24 12:27:21 +0530

    Day13

---
+++ Mode1/BankProject/src/com/hcl/bank/AccountBal.java
@@ -0,0 +1,36 @@
+package com.hcl.bank;
+
+public class AccountBal {
+	public static String WithdrawAccountBal(int accountNo,int WithdrawAmount){
+		return new AccountDao().WithdrawAccountDao(accountNo, WithdrawAmount);
+		
+	}
+	public static String depositAccountBal(int accountNo,int depositAmount) {
+		return new AccountDao().depositAccountDao(accountNo, depositAmount);
+		
+		
+	}
+	public static String closeAccountBal(int accountNo){
+		return new AccountDao().closeAccountDao(accountNo);
+		
+	}
+	public static String updateAccountBal(int accountNo,String city,String state ) {
+		return new AccountDao().updateAccountDao(accountNo,city,state);
+		
+	}
+	public static Accounts searchAccountBal(int accountNo) {
+		return new AccountDao().searchAccountDao(accountNo);
+		
+	}
+	public static String createAccountBal(Accounts objAccounts) {
+		return new AccountDao().createAccountDao(objAccounts);
+		
+		
+	}
+
+	public static int generateAccountBal(){
+		return new AccountDao().generateAccountNoDao();
+		
+	}
+
+}
